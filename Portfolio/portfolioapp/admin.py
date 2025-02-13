99e3822b0311215c9da8c7c537822a26be8133e3
opokharel1 <opokharel003@gmail.com>
2025-02-13 12:18:01 +0545

    database rendering in homepage

--- Portfolio/portfolioapp/admin.py
+++ Portfolio/portfolioapp/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from portfolioapp.models import Education
-from portfolioapp.models import Experience
+from portfolioapp.models import Education, Experience
+# from portfolioapp.models import * --> yesto gare, models.py ma bhayeko sabai classes import hunxa
+
 
 # Register your models here.
 
--- Portfolio/portfolioapp/views.py
+++ Portfolio/portfolioapp/views.py
@@ -1,10 +1,16 @@
 
 from django.shortcuts import render
+from portfolioapp.models import Education, Experience
 
 # Create your views here.
 
 def homepage_view(request):
-    return render(request, "homepage.html")
+    educations =Education.objects.all()
+    experiences= Experience.objects.all()  #--> yo ORM le garda code yesto sajilo bhako, natra, SQL lekhna parthyo
+    context= {
+        "educations": educations, 
+        "experiences": experiences}
+    return render(request, "homepage.html", context=context)
 
 def about_view(request):
     return render(request, "about.html")
